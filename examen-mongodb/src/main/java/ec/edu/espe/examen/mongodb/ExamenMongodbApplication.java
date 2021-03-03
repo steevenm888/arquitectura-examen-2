be4c2608a2435e8aa1a77e9ff5dd8749661e1304
steevenm888 <steevenm888@gmail.com>
2021-03-03 17:54:16 -0500

    added the 2 project structures

---
+++ examen-mongodb/src/main/java/ec/edu/espe/examen/mongodb/ExamenMongodbApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenMongodbApplication.class, args);
+	}
+
+}
---
+++ examen-mongodb/src/test/java/ec/edu/espe/examen/mongodb/ExamenMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examen.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
